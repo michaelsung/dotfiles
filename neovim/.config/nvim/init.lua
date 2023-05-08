66e27bf659d2bda836df2f107f95203f499fe167
michaelsung <michaelsung@users.noreply.github.com>
2023-05-08 23:36:46 +1000

    add close buffer bind

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -2,6 +2,7 @@
 vim.g.mapleader = " "
 -- vim.keymap.set("n", "<leader>a", function() print "hi" end, { desc = "test leader bind" })
 -- vim.keymap.set("n", "<leader>pv", "<Cmd>Vex<CR>", { desc = "NetRW (Vertical split)" })
+vim.keymap.set("n", "<leader>pq", "<Cmd>bp|bd #<CR>", { desc = "Buffer close" })
 
 -- vim settings - editor
 vim.opt.number = true
