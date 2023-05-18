410420e2f138a81632e99af9b9bd1be273218c48
michaelsung <michaelsung@users.noreply.github.com>
2023-05-18 13:31:05 +1000

    add close all buffers except current keymap

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,4 @@ vim.g.mapleader = " "
 -- vim.keymap.set("n", "<leader>a", function() print "hi" end, { desc = "test leader bind" })
 -- vim.keymap.set("n", "<leader>pv", "<Cmd>Vex<CR>", { desc = "NetRW (Vertical split)" })
 vim.keymap.set("n", "<leader>bq", "<Cmd>bp|bd #<CR>", { desc = "Buffer close" })
+vim.keymap.set("n", "<leader>ba", "<Cmd>%bd|e#|bd#<CR>", { desc = "Close all buffers except current" })
