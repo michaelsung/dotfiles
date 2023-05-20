69141273997e8038c7d06a972d80dd8572694583
michaelsung <michaelsung@users.noreply.github.com>
2023-05-20 17:56:11 +1000

    move gitsigns to workflow.lua

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -8,61 +8,6 @@ return {
         end,
     },
     {
-        "lewis6991/gitsigns.nvim",
-        config = function()
-            require("gitsigns").setup({
-                current_line_blame = true,
-                on_attach = function(bufnr)
-                    local gs = package.loaded.gitsigns
-
-                    local function map(mode, l, r, opts)
-                        opts = opts or {}
-                        opts.buffer = bufnr
-                        vim.keymap.set(mode, l, r, opts)
-                    end
-
-                    -- Navigation
-                    map('n', ']c', function()
-                        if vim.wo.diff then return ']c' end
-                        vim.schedule(function() gs.next_hunk() end)
-                        return '<Ignore>'
-                    end, { expr = true, desc = "Git: next hunk" })
-
-                    map('n', '[c', function()
-                        if vim.wo.diff then return '[c' end
-                        vim.schedule(function() gs.prev_hunk() end)
-                        return '<Ignore>'
-                    end, { expr = true, desc = "Git: previous hunk" })
-
-                    -- Register mapping group in which-key
-                    local wk = require("which-key")
-                    wk.register({ ["<leader>"] = { g = { name = " Gitsigns" } } })
-
-                    -- Actions
-                    map('n', '<leader>gs', gs.stage_hunk, { desc = "Stage hunk" })
-                    map('n', '<leader>gr', gs.reset_hunk, { desc = "Reset hunk" })
-                    map('v', '<leader>gs', function() gs.stage_hunk { vim.fn.line("."), vim.fn.line("v") } end,
-                        { desc = "Stage hunk" })
-                    map('v', '<leader>gr', function() gs.reset_hunk { vim.fn.line("."), vim.fn.line("v") } end,
-                        { desc = "Reset hunk" })
-
-                    map('n', '<leader>gS', gs.stage_buffer, { desc = "Stage buffer" })
-                    map('n', '<leader>gu', gs.undo_stage_hunk, { desc = "Undo stage hunk" })
-                    map('n', '<leader>gR', gs.reset_buffer, { desc = "Reset buffer" })
-                    map('n', '<leader>gp', gs.preview_hunk, { desc = "Preview hunk" })
-                    map('n', '<leader>gb', function() gs.blame_line { full = true } end, { desc = "Blame line" })
-                    map('n', '<leader>gl', gs.toggle_current_line_blame, { desc = "Toggle current line blame" })
-                    map('n', '<leader>gt', gs.diffthis, { desc = "Diff this" })
-                    map('n', '<leader>gT', function() gs.diffthis('~') end, { desc = "Diff this ~" })
-                    map('n', '<leader>gd', gs.toggle_deleted, { desc = "Toggle deleted" })
-
-                    -- Text object
-                    map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-                end
-            })
-        end,
-    },
-    {
         'echasnovski/mini.statusline',
         config = function()
             require('mini.statusline').setup()
