a547c53303da1b93127ae05510bcfd891d19d710
michaelsung <michaelsung@users.noreply.github.com>
2023-05-20 23:39:22 +1000

    cmp completion borders

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -50,14 +50,21 @@ return {
 
             lsp.setup()
             local cmp = require('cmp')
+            local cmp_action = require('lsp-zero').cmp_action()
             cmp.setup({
                 mapping = {
                     ['<CR>'] = cmp.mapping.confirm({ select = false }),
+                    ['<Tab>'] = cmp_action.tab_complete(),
+                    ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
                 },
                 preselect = 'item',
                 completion = {
                     completeopt = 'menu,menuone,noinsert'
                 },
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                }
             })
             -- Disable tsserver formatting to prevent overriding .eslintrc / .prettierrc
             require('lspconfig').tsserver.setup({
