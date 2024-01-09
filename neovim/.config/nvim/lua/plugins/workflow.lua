cb75c350bee35c6f51fb28c5bd7d9ac52d6aad3a
michaelsung <19608130+michaelsung@users.noreply.github.com>
2024-01-09 23:37:33 +1100

    neovim: add git blame

---
+++ neovim/.config/nvim/lua/plugins/workflow.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
+        delay = 0,
+      },
+    },
+  },
+}
