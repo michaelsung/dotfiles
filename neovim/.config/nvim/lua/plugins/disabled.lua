47e01c7dc23ac05515dfd220b2a6d0e76c9db404
michaelsung <michaelsung@users.noreply.github.com>
2024-01-26 20:29:16 +1100

    Remove disabled plugins

--- neovim/.config/nvim/lua/plugins/disabled.lua
+++ neovim/.config/nvim/lua/plugins/disabled.lua
@@ -1,10 +1,5 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    enabled = false,
-  },
   { "folke/noice.nvim", enabled = false },
-  -- { "rcarriga/nvim-notify", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
   { "goolord/alpha-nvim", enabled = false },
 }
